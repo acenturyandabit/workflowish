e0d886a8bb702c01de1e4838c852a2629a3a94b9
acenturyandabit <steeven.liu2@gmail.com>
2023-04-01 21:36:45 +1100

    Version 2.4: Progressive Web App

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,41 @@
 import tsconfigPaths from 'vite-tsconfig-paths'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default {
-    plugins: [tsconfigPaths()],
+    plugins: [
+        tsconfigPaths(),
+        VitePWA({
+            registerType: 'autoUpdate',
+            strategies: "generateSW",
+            injectRegister: 'auto',
+            // Kudos to the service provided by https://realfavicongenerator.net/.
+            includeAssets: ["icon-128.png"],
+            manifest: {
+                name: 'Polymorph',
+                short_name: 'Polymorph',
+                description: 'Recursive List Todo App.',
+                theme_color: '#ffffff',
+                icons: [
+                    {
+                        "src": "/android-chrome-192x192.png",
+                        "sizes": "192x192",
+                        "type": "image/png"
+                    },
+                    {
+                        "src": "/android-chrome-512x512.png",
+                        "sizes": "512x512",
+                        "type": "image/png",
+                        "purpose": "any maskable"
+                    }
+                ],
+                background_color: "#ffffff",
+                display: "standalone"
+            },
+            devOptions: {
+                enabled: true
+            }
+        })
+    ],
     resolve: {
         alias: {
             // Solve sanitize-html complaints
